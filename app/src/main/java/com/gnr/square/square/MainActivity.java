804941ad9594e824e99fe1a87459f1fccfe9139c
prottoy <prottoy2007@gmail.com>
2015-01-29 16:58:41 +0600

    GNR square version 1 ready

--- app/src/main/java/com/gnr/square/square/MainActivity.java
+++ app/src/main/java/com/gnr/square/square/MainActivity.java
@@ -1,8 +1,11 @@
 package com.gnr.square.square;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,6 +15,7 @@ import android.widget.Button;
 import android.widget.Toast;
 import android.view.View.OnClickListener;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
@@ -20,12 +24,30 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.ByteArrayBuffer;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class MainActivity extends ActionBarActivity {
+    public static final String MY_PREFS_NAME = "gnrcredentials";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,27 +57,53 @@ public class MainActivity extends ActionBarActivity {
 
         loadSquareWebsite();
 
+
         Button changeViewBtn = (Button) findViewById(R.id.changeviewbtn);
         changeViewBtn.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), Settings.class);
-                view.getContext().startActivity(i);
+                showSettings(view);
             }
 
         });
 
         Button checkInBtn = (Button) findViewById(R.id.checkinbtn);
         checkInBtn.setOnClickListener(new OnClickListener() {
-
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Checking in!",
-                        Toast.LENGTH_LONG).show();
-            }
 
+                SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+                String restoredText = prefs.getString("name", null);
+
+                if (restoredText != null) {
+                    String name = prefs.getString("name", "");
+                    String email = prefs.getString("email", "");
+
+                    if (name.length()>0 && email.length()>0) {
+                        Toast.makeText(getApplicationContext(), "CHECKING IN!",
+                                Toast.LENGTH_LONG).show();
+
+                        Checkin cin= new Checkin();
+                        cin.execute(name, email);
+                    }else{
+                        Toast.makeText(getApplicationContext(), "PLEASE SET YOUR NAME AND EMAIL FIRST!",
+                                Toast.LENGTH_LONG).show();
+                        showSettings(view);
+                    }
+                }else{
+                    Toast.makeText(getApplicationContext(), "PLEASE SET YOUR NAME AND EMAIL FIRST!",
+                            Toast.LENGTH_LONG).show();
+                    showSettings(view);
+                }
+
+            }
         });
+    }
+
+    private void showSettings(View view) {
+        Intent i = new Intent(getApplicationContext(), Settings.class);
+        view.getContext().startActivity(i);
     }
 
     private void loadSquareWebsite() {
@@ -87,8 +135,62 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void postData() {
 
+    //Private AsyncTask class for checking in
+    private class Checkin extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String response= postData(params[0], params[1]);
+            String value= null;
+            try {
+                JSONObject jObj= new JSONObject(response);
+                value= (String)jObj.get("response");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            Log.d("response", response);
+
+            return value;
+        }
+
+        protected void onPostExecute(String result) {
+            Toast.makeText(getApplicationContext(), result,
+                    Toast.LENGTH_LONG).show();
+        }
+
+
+        public String postData(String name, String email) {
+            String jresponse= null;
+
+            // Create a new HttpClient and Post Header
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(
+                    "http://www.green-red.com/square/checkin");
+
+            try {
+                // Add your data
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                nameValuePairs.add(new BasicNameValuePair("name",
+                        name));
+                nameValuePairs.add(new BasicNameValuePair("email",
+                        email));
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                // Execute HTTP Post Request
+                HttpResponse response = httpclient.execute(httppost);
+
+                String json_string = EntityUtils.toString(response.getEntity());
+                jresponse= json_string;
+
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+            }
+            return jresponse;
+        }
     }
-
 }
--- app/src/main/java/com/gnr/square/square/Settings.java
+++ app/src/main/java/com/gnr/square/square/Settings.java
@@ -1,16 +1,20 @@
 package com.gnr.square.square;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
 public class Settings extends ActionBarActivity {
+    public static final String MY_PREFS_NAME = "gnrcredentials";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +30,44 @@ public class Settings extends ActionBarActivity {
             }
         });
 
+        EditText nameText   = (EditText)findViewById(R.id.nameText);
+        EditText emailText   = (EditText)findViewById(R.id.emailText);
+
+        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        String restoredText = prefs.getString("name", null);
+        if (restoredText != null) {
+            String name = prefs.getString("name", "");
+            String email = prefs.getString("email", "");
+
+            if (name.length()>0 && email.length()>0) {
+                nameText.setHint(name);
+                emailText.setHint(email);
+            }
+            Toast.makeText(getApplicationContext(), "Name: "+ name + " Email:"+ email,
+                    Toast.LENGTH_LONG).show();
+        }
+
         Button saveBtn = (Button) findViewById(R.id.savebtn);
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Saving!",
-                        Toast.LENGTH_LONG).show();
+                EditText nameText   = (EditText)findViewById(R.id.nameText);
+                EditText emailText   = (EditText)findViewById(R.id.emailText);
+
+                if (nameText.length()>0 && emailText.length()>0) {
+                    SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+                    editor.putString("name", nameText.getText().toString());
+                    editor.putString("email", emailText.getText().toString());
+                    editor.commit();
+
+                    Toast.makeText(getApplicationContext(), "NAME AND EMAIL SAVED!",
+                            Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "PLEASE ENTER BOTH NAME AND EMAIL.",
+                            Toast.LENGTH_LONG).show();
+                }
+
             }
         });
     }
